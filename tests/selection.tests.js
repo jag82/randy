2e2a2f9c7a12b00e1612560f567aa045075c923f
Jag <chadha@toechtersoehne.com>
2018-02-05 15:10:38 +0100

    experimenting with travis

--- tests/selection.tests.js
+++ tests/selection.tests.js
@@ -6,12 +6,12 @@ const Randy = require('./../randy.js');
 
 // SELECTION TESTS
 // 
-test.skip('', function(t) {
-    const r = new Ran2dy();
+// test.skip('', function(t) {
+//     const r = new Ran2dy();
 
-    t.deepEqual(r.items, [ ]);
-    t.ok(r.options);
-    t.end();
-});
+//     t.deepEqual(r.items, [ ]);
+//     t.ok(r.options);
+//     t.end();
+// });
 
 
